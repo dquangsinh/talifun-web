f88ad6d2a3105ddb61a4ea888c409676d50506a8
taliesinsisson <taliesinsisson@94e080da-ce3a-11de-958f-af26545d0b1a>
2009-11-20 15:13:53 +0000

    Added a module to provide authorization based on urls matching regular expressions defined in web.config

---
+++ Talifun.Web/RegexUrlAuthorization/Config/CurrentRegexUrlAuthorizationConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Talifun.Web.Configuration;
+
+namespace Talifun.Web.RegexUrlAuthorization.Config
+{
+    /// <summary>
+    /// Provides easy access to the current application configuration.
+    /// </summary>
+    public static class CurrentRegexUrlAuthorizationConfiguration
+    {
+        /// <summary>
+        /// Gets the static instance of <see cref="RegexUrlAuthorizationSection" /> representing the current application configuration.
+        /// </summary>
+        public static RegexUrlAuthorizationSection Current
+        {
+            get { return CurrentConfigurationManager.GetSection<RegexUrlAuthorizationSection>(); }
+        }
+    }
+}
